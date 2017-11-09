b3dbd3ea90d744d5183bd9233ed12e0c0719d36c
Rito <vukzrito@gmail.com>
2017-11-09 20:37:25 +0200

    Fi Receive Staff action creator test

--- src/features/institution/wages/duck.test.js
+++ src/features/institution/wages/duck.test.js
@@ -1,107 +1,103 @@
 import * as imports from "./duck.js";
-import { sampleStore } from "../../models/sampleStore";
-
+import { sampleStore } from "../../../models/sampleStore";
 
 //Reducer tests
-const initialState= {
+const initialState = {
   uiConfig: imports.uiConfigInitialState,
   coaches: {},
-  loadingStatus: loadingStatusInitialState,
+  loadingStatus: imports.loadingStatusInitialState,
   coachWages: {}
 };
-describe("Reducers", ()=>{
-  const {coachesReducer} = imports;
+describe("Reducers", () => {
+  const { wagesReducer } = imports;
   describe("coachesReducer", () => {
-    describe("RECEIVE_STAFF", ()=>{
-      const {RECEIVE_STAFF} = imports;
+    describe("RECEIVE_STAFF", () => {
+      const { RECEIVE_STAFF } = imports;
       const action = {
-        type:RECEIVE_STAFF,
-        payload:{
-          coaches:{}
+        type: RECEIVE_STAFF,
+        payload: {
+          coaches: {}
         }
       };
       const newState = wagesReducer(initialState, action);
       expect(newState.uiConfig.coaches).toEqual(action.payload.coaches);
     });
-
   });
 
-  const {loadingStatusReducer} = imports;
+  const { loadingStatusReducer } = imports;
   describe("loadingStatusReducer", () => {
-    describe("REQUEST_STAFF", ()=>{
-      const {REQUEST_STAFF} = imports;
+    describe("REQUEST_STAFF", () => {
+      const { REQUEST_STAFF } = imports;
       const action = {
-        type:REQUEST_STAFF
+        type: REQUEST_STAFF
       };
       const newState = wagesReducer(initialState, action);
       expect(newState.loadingStatus.isStaffLoading).ToBeTruthy();
     });
-    describe("RECEIVE_STAFF", ()=>{
-      const {RECEIVE_STAFF} = imports;
+    describe("RECEIVE_STAFF", () => {
+      const { RECEIVE_STAFF } = imports;
       const action = {
-        type:RECEIVE_STAFF
+        type: RECEIVE_STAFF
       };
       const newState = wagesReducer(initialState, action);
       expect(newState.loadingStatus.isStaffLoading).ToBeFalsy();
     });
 
-    describe("ERROR_LOADING_STAFF", ()=>{
-      const {ERROR_LOADING_STAFF} = imports;
+    describe("ERROR_LOADING_STAFF", () => {
+      const { ERROR_LOADING_STAFF } = imports;
       const action = {
-        type:ERROR_LOADING_STAFF
+        type: ERROR_LOADING_STAFF
       };
       const newState = wagesReducer(initialState, action);
       expect(newState.loadingStatus.isStaffLoading).ToBeFalsy();
     });
 
-    describe("REQUEST_WAGES", ()=>{
-      const {REQUEST_WAGES} = imports;
+    describe("REQUEST_WAGES", () => {
+      const { REQUEST_WAGES } = imports;
       const action = {
-        type:REQUEST_WAGES
+        type: REQUEST_WAGES
       };
       const newState = wagesReducer(initialState, action);
       expect(newState.loadingStatus.isWagesLoading).ToBeTruthy();
     });
 
-    describe("RECEIVE_WAGES", ()=>{
-      const {RECEIVE_WAGES} = imports;
+    describe("RECEIVE_WAGES", () => {
+      const { RECEIVE_WAGES } = imports;
       const action = {
-        type:RECEIVE_WAGES
+        type: RECEIVE_WAGES
       };
       const newState = wagesReducer(initialState, action);
       expect(newState.loadingStatus.isWagesLoading).ToBeFalsy();
     });
 
-    describe("ERROR_LOADING_WAGES", ()=>{
-      const {ERROR_LOADING_WAGES} = imports;
+    describe("ERROR_LOADING_WAGES", () => {
+      const { ERROR_LOADING_WAGES } = imports;
       const action = {
-        type:ERROR_LOADING_WAGES
+        type: ERROR_LOADING_WAGES
       };
       const newState = wagesReducer(initialState, action);
       expect(newState.loadingStatus.isWagesLoading).ToBeFalsy();
     });
-
   });
 
-  const {coachWagesReducer} = imports;
-  describe("coachWagesReducer", ()=>{
-      describe("RECEIVE_WAGES", ()=>{
-          const {RECEIVE_WAGES} =imports;
-          const action = {
-            type:RECEIVE_WAGES,
-            payload:{wages:{}}
-          };
+  const { coachWagesReducer } = imports;
+  describe("coachWagesReducer", () => {
+    describe("RECEIVE_WAGES", () => {
+      const { RECEIVE_WAGES } = imports;
+      const action = {
+        type: RECEIVE_WAGES,
+        payload: { wages: {} }
+      };
 
-          const newState= wagesReducer(initialState, action);
-          expect(newState).toEqual(action.payload.wages);
-      });
+      const newState = wagesReducer(initialState, action);
+      expect(newState).toEqual(action.payload.wages);
+    });
   });
 });
 
 //Action creator tests
 describe("Action Creators", () => {
   describe("Synchonous functions", () => {
-   
     describe("requestStaff", () => {
       const { requestStaff, REQUEST_STAFF } = imports;
       test("Returns the correct action", () => {
@@ -116,11 +112,11 @@ describe("Action Creators", () => {
     describe("receiveStaff", () => {
       const { receiveStaff, RECEIVE_STAFF } = imports;
       test("Returns the correct action", () => {
-        var coaches = [];
+        var coaches = {};
         const createdAction = receiveStaff(coaches);
         const expectedAction = {
           type: RECEIVE_STAFF,
-          payload:{
+          payload: {
             coaches
           }
         };
@@ -131,11 +127,11 @@ describe("Action Creators", () => {
     describe("errorLoadingStaff", () => {
       const { errorLoadingStaff, ERROR_LOADING_STAFF } = imports;
       test("Returns the correct action", () => {
-        var error = { code: "ERROR_LOADING", message: "Failed to load"};
+        var error = { code: "ERROR_LOADING", message: "Failed to load" };
         const createdAction = errorLoadingStaff(error);
         const expectedAction = {
           type: ERROR_LOADING_STAFF,
-          payload:{
+          payload: {
             error
           }
         };
@@ -166,19 +162,16 @@ describe("Action Creators", () => {
           title: "U/15 C Athletics Girls Practice",
           type: "HOURLY",
           wage: 450
-
         };
         const createdAction = receiveWages(wages);
         const expectedAction = {
           type: RECEIVE_WAGES,
-          payload:{
+          payload: {
             wages
           }
         };
         expect(createdAction).toEqual(expectedAction);
       });
     });
-
   });
 });
-
