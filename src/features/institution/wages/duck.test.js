8cb587181827af6e48c9d19d1be48151ded67b17
= <=>
2017-11-09 18:39:34 +0200

    Add action creator tests

--- src/features/institution/wages/duck.test.js
+++ src/features/institution/wages/duck.test.js
@@ -97,3 +97,88 @@ describe("Reducers", ()=>{
       });
   });
 });
+
+//Action creator tests
+describe("Action Creators", () => {
+  describe("Synchonous functions", () => {
+   
+    describe("requestStaff", () => {
+      const { requestStaff, REQUEST_STAFF } = imports;
+      test("Returns the correct action", () => {
+        const createdAction = requestStaff();
+        const expectedAction = {
+          type: REQUEST_STAFF
+        };
+        expect(createdAction).toEqual(expectedAction);
+      });
+    });
+
+    describe("receiveStaff", () => {
+      const { receiveStaff, RECEIVE_STAFF } = imports;
+      test("Returns the correct action", () => {
+        var coaches = [];
+        const createdAction = receiveStaff(coaches);
+        const expectedAction = {
+          type: RECEIVE_STAFF,
+          payload:{
+            coaches
+          }
+        };
+        expect(createdAction).toEqual(expectedAction);
+      });
+    });
+
+    describe("errorLoadingStaff", () => {
+      const { errorLoadingStaff, ERROR_LOADING_STAFF } = imports;
+      test("Returns the correct action", () => {
+        var error = { code: "ERROR_LOADING", message: "Failed to load"};
+        const createdAction = errorLoadingStaff(error);
+        const expectedAction = {
+          type: ERROR_LOADING_STAFF,
+          payload:{
+            error
+          }
+        };
+        expect(createdAction).toEqual(expectedAction);
+      });
+    });
+
+    describe("requestWages", () => {
+      const { requestWages, REQUEST_WAGES } = imports;
+      test("Returns the correct action", () => {
+        const createdAction = requestWages();
+        const expectedAction = {
+          type: REQUEST_WAGES
+        };
+        expect(createdAction).toEqual(expectedAction);
+      });
+    });
+
+    describe("receiveWages", () => {
+      const { receiveWages, RECEIVE_WAGES } = imports;
+      test("Returns the correct action", () => {
+        var wages = {
+          date: "2017-10-26",
+          hours: {
+            overtime: 0,
+            standard: 3
+          },
+          title: "U/15 C Athletics Girls Practice",
+          type: "HOURLY",
+          wage: 450
+
+        };
+        const createdAction = receiveWages(wages);
+        const expectedAction = {
+          type: RECEIVE_WAGES,
+          payload:{
+            wages
+          }
+        };
+        expect(createdAction).toEqual(expectedAction);
+      });
+    });
+
+  });
+});
+
