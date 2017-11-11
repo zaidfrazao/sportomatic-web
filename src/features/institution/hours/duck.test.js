16a7998dc591ad74f74e122176a234c72eeac6cc
Rito <vukzrito@gmail.com>
2017-11-11 09:04:15 +0200

    Test coaches reducer

--- src/features/institution/hours/duck.test.js
+++ src/features/institution/hours/duck.test.js
@@ -24,4 +24,28 @@ describe("Reducers", () => {
       });
     });
   });
+
+  describe("coachesReducer", () => {
+    describe("RECEIVE_STAFF", () => {
+      const { RECEIVE_STAFF } = imports;
+      test("Receives coaches", () => {
+        const action = {
+          type: RECEIVE_STAFF,
+          payload: {
+            coaches: {
+              Bhmq7MvEEbawZJE6xnxqsXn4dWG2: {
+                name: "Rowan",
+                phoneNumber: "(084) 291-0482",
+                profilePictureURL:
+                  "https://firebasestorage.googleapis.com/v0/b/sportomatic-beta.appspot.com/o/coach%2FBhmq7MvEEbawZJE6xnxqsXn4dWG2%2Fprofile-picture.jpg?alt=media&token=88168b80-e085-4e52-b504-0357d0a0dcc7",
+                surname: "Walker-Campbell"
+              }
+            }
+          }
+        };
+        const newState = hoursReducer(initialState, action);
+        expect(newState.coaches).toEqual(action.payload.coaches);
+      });
+    });
+  });
 });
