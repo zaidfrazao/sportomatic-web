4f5642a6c2bacbdce59f077fef2281432be04ba2
Zaid Frazao <zaidfrazao@gmail.com>
2017-12-05 12:02:03 +0200

    Revert "Merge branch 'feature/clean-sign-in' into feature/move-to-firestore"
    This reverts commit db87b852f65d5d233d169a440f6d1a28bc04aebb, reversing
    changes made to 4e43d701b5f172af4f132df135c826f4ba0af3f2.

--- src/components/NotificationModal/index.jsx
+++ src/components/NotificationModal/index.jsx
@@ -25,7 +25,7 @@ class NotificationModal extends Component {
   };
 
   render() {
-    const { isOpen, message, heading, handleOkClick } = this.props;
+    const { isOpen, handleOkClick, message, heading } = this.props;
 
     return (
       <Dialog open={isOpen}>
--- src/features/sign-in/components/PasswordResetDialog/index.jsx
+++ src/features/sign-in/components/PasswordResetDialog/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withStyles } from "material-ui/styles";
 import Dialog, {
   DialogActions,
   DialogContent,
@@ -8,7 +9,6 @@ import Button from "material-ui/Button";
 import { CircularProgress } from "material-ui/Progress";
 import TextField from "material-ui/TextField";
 import Typography from "material-ui/Typography";
-import { withStyles } from "material-ui/styles";
 
 const styles = {
   emailTextField: {
@@ -16,38 +16,42 @@ const styles = {
   }
 };
 
-type Props = {
-  isOpen: boolean,
-  email: string,
-  isLoading: boolean,
-  actions: {
+class PasswordResetDialog extends Component {
+  props: {
+    isOpen: boolean,
     closeDialog: () => void,
     sendEmail: string => void,
-    updateEmail: string => void
-  }
-};
+    email: string,
+    updateEmail: string => void,
+    isLoading: boolean
+  };
 
-class PasswordResetDialog extends Component<Props> {
   static defaultProps = {
     isOpen: false,
+    closeDialog: () => {
+      console.log("Password reset dialog was closed.");
+    },
+    sendEmail: email => {
+      console.log(`Password reset email sent to "${email}".`);
+    },
     email: "default@email.com",
-    isLoading: true,
-    actions: {
-      closeDialog: () => {
-        console.log("Password reset dialog was closed.");
-      },
-      sendEmail: email => {
-        console.log(`Password reset email sent to "${email}".`);
-      },
-      updateEmail: newEmail => {
-        console.log(`Password reset email address updated to "${newEmail}".`);
-      }
-    }
+    updateEmail: newEmail => {
+      console.log(`Password reset email address updated to "${newEmail}".`);
+    },
+    isLoading: true
   };
 
   render() {
-    const { classes, isOpen, email, isLoading, emailError } = this.props;
-    const { closeDialog, sendEmail, updateEmail } = this.props.actions;
+    const {
+      classes,
+      isOpen,
+      closeDialog,
+      sendEmail,
+      email,
+      updateEmail,
+      isLoading,
+      emailError
+    } = this.props;
 
     return (
       <Dialog open={isOpen} onRequestClose={() => closeDialog()}>
