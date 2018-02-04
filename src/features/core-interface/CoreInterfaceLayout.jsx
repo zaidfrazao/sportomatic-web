5ea0287fd9144d053ba46ec892b10eea79b43604
Zaid Frazao <zaidfrazao@gmail.com>
2018-02-04 10:55:41 +0200

    Update version number

--- src/features/core-interface/CoreInterfaceLayout.jsx
+++ src/features/core-interface/CoreInterfaceLayout.jsx
@@ -280,7 +280,7 @@ class CoreInterfaceLayout extends Component {
             }}
             isMobile={isMobile}
             feature={appBarTitle}
-            versionNumber="0.9.2"
+            versionNumber="0.9.3"
             role={role}
           />
           <div className={classes.content}>
