74ea087a83cef6d1eb0ba0817708abdb27b07b87
Zaid Frazao <zaidfrazao@gmail.com>
2018-05-21 13:16:34 +0200

    Adjust loader size

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -4,11 +4,11 @@ import { grey, lightBlue } from "../../utils/colours";
 
 const styles = theme => ({
   loader: {
-    border: `6px solid ${grey[200]}`,
-    borderTop: `6px solid ${lightBlue[500]}`,
+    border: `4px solid ${grey[200]}`,
+    borderTop: `4px solid ${lightBlue[500]}`,
     borderRadius: "50%",
-    width: 28,
-    height: 28,
+    width: 20,
+    height: 20,
     margin: 12,
     animation: "spin 2s linear infinite"
   },
