df2c7097295647c45f1fd7b3b190a1897b6e699a
Zaid Frazao <zaidfrazao@gmail.com>
2018-06-05 14:43:49 +0200

    Update version number to 0.9.7

--- src/features/core-interface/CoreInterfaceLayout.jsx
+++ src/features/core-interface/CoreInterfaceLayout.jsx
@@ -436,7 +436,7 @@ class CoreInterfaceLayout extends Component {
     const featureName = pathname.split("/")[2];
     const isMobile = windowWidth < 800;
     const isTablet = windowWidth < 1080;
-    const versionNumber = "0.9.6";
+    const versionNumber = "0.9.7";
 
     const communityRole = this.getRole();
     const userFirstName = accountInfo.info.name;
