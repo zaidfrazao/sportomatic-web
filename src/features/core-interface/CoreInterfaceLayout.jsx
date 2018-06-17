031e2187a5d86754021039c49fe90006232ba39f
Zaid Frazao <zaidfrazao@gmail.com>
2018-06-17 21:03:19 +0200

    Update to version 0.9.16

--- src/features/core-interface/CoreInterfaceLayout.jsx
+++ src/features/core-interface/CoreInterfaceLayout.jsx
@@ -439,7 +439,7 @@ class CoreInterfaceLayout extends Component {
     const allowedSports = ["Hockey", "Netball", "Rugby"];
     const isMobile = windowWidth < 800;
     const isTablet = windowWidth < 1080;
-    const versionNumber = "0.9.15";
+    const versionNumber = "0.9.16";
 
     const communityRole = this.getRole();
     const userFirstName = accountInfo.info.name;
